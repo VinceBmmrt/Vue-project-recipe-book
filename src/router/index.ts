e88c40b7109a204fa97b1fbe8afc9f9c09088285
VinceBmmrt <vincebommert75@gmail.com>
2025-01-23 18:32:18 +0100

    Add dynamic routes, fallback route, and programmatic navigation
    - Implement dynamic routing for recipe details using '/recipe/:id' with alias '/meal/:id'.
    - Add a fallback route to handle 404 errors using NotFoundView.
    - Set up programmatic navigation and redirection from '/home' to '/'.
    - Ensure proper usage of <router-link> for navigation between views.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import AddRecipeView from '@/views/AddRecipeView.vue';
+import FavoritesView from '@/views/FavoritesView.vue';
+import NotFoundView from '@/views/NotFoundView.vue';
+import RecipeView from '@/views/RecipeView.vue';
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -9,14 +13,31 @@ const router = createRouter({
       component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/home',
+      redirect: '/',
+    },
+    {
+      path: '/favorites',
+      name: 'favorites',
+      component: FavoritesView,
+    },
+    {
+      path: '/add-recipe',
+      name: 'add-recipe',
+      component: AddRecipeView,
+    },
+    {
+      path: '/recipe/:id',
+      name: 'recipe',
+      component: RecipeView,
+      alias: '/meal/:id',
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'not-found',
+      component: NotFoundView,
     },
   ],
-})
+});
 
-export default router
+export default router;
---
+++ src/views/RecipeView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>Recipe {{ $route.params.id }}</div>
+  <nav>
+    <ul>
+      <li>
+        <RouterLink :to="{ name: 'recipe', params: { id: 1 } }">Recipe1</RouterLink>
+      </li>
+      <li>
+        <RouterLink :to="{ name: 'recipe', params: { id: 2 } }">Recipe2</RouterLink>
+      </li>
+      <li>
+        <RouterLink :to="{ name: 'recipe', params: { id: 3 } }">Recipe3</RouterLink>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script setup lang="ts">
+import { watch } from 'vue';
+import { RouterLink, useRoute } from 'vue-router';
+const route = useRoute();
+console.log('🚀 ~ route:', route);
+
+watch(
+  () => route.params.id,
+  () => console.log('Fetching inside watch'),
+  {
+    immediate: true,
+  },
+);
+</script>
